cc704f59638264bf553bff9d47cea60979af0904
retr0 <aligholami000777@gmail.com>
2026-07-27 16:25:08 +0330

    commit 2

---
+++ finder-app/writer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Error: Two arguments required"
+    exit 1
+fi
+
+writefile=$1
+writestr=$2
+
+mkdir -p "$(dirname "$writefile")"
+
+echo "$writestr" > "$writefile"
+if [ $? -ne 0 ]; then
+    echo "Error: File could not be created"
+    exit 1
+fi
+
