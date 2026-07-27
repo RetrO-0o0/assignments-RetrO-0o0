939267cec8f325c40e8c96f129aeb2a802479876
retr0 <aligholami000777@gmail.com>
2026-07-28 01:00:09 +0330

    rewriting the writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Error: Two arguments required"
-    exit 1
+if [ $# -ne 2 ]
+then
+	echo "arguments are not specified"
+	exit 1
 fi
 
-writefile=$1
-writestr=$2
+writestr="$2"
+writefile="$1"
 
-mkdir -p "$(dirname "$writefile")"
+if [ -w "$writefile" ]
+then
+	echo $writestr > $writefile
+else
+	mkdir -p "$(dirname "$writefile")"
+	if [ $? -ne 0 ]
+	then
+		echo "error creating the directory"
+		exit 1
+	fi
 
-echo "$writestr" > "$writefile"
-if [ $? -ne 0 ]; then
-    echo "Error: File could not be created"
-    exit 1
+	echo $writestr > $writefile
+	if [ $? -ne 0 ]
+	then
+		echo "error writing into the file"
+		exit 1
+	fi
 fi
 
+exit 0
