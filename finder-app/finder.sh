2ac1e2668eb20d5fc6b8e3cd9772011607db124b
retr0 <aligholami000777@gmail.com>
2026-07-28 19:18:18 +0330

    fixing bugs in finder.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,1 +1,23 @@
-./finder.sh
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+	echo "2 arguments needed"
+	exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d $filesdir ]
+then
+	echo "$filesdir is not a directory"
+	exit 1
+fi
+
+numfiles=$(find "$filesdir" -type f | wc -l)
+numlines=$(grep -r "$searchstr" "$filesdir" | wc -l)
+
+echo "The number of files are ${numfiles} and the number of matching lines are ${numlines}"
+
+exit 0
